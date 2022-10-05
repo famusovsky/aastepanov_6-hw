3aaf47c5ba5e3880ac5e16043c5b6723195e5a2d
famusovsky <st3p-by-step@ya.ru>
2022-10-05 12:38:58 +0300

    Initial Commit

---
+++ aastepanov_6 HW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aastepanov_6 HW2
+//
+//  Created by Алексей Степанов on 2022-10-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
