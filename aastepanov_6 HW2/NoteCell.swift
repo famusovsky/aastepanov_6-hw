f1e857c554b150e8b87d47c6858fd458fea8e1ca
Alexey Stepanov <st3p-by-step@ya.ru>
2022-11-15 16:13:17 +0300

    Started doing hw4

---
+++ aastepanov_6 HW2/NoteCell.swift
@@ -0,0 +1,8 @@
+//
+//  NoteCell.swift
+//  aastepanov_6 HW2
+//
+//  Created by Алексей Степанов on 2022-11-14.
+//
+
+import Foundation
---
+++ aastepanov_6 HW2/ShortNote.swift
@@ -0,0 +1,8 @@
+//
+//  ShortNote.swift
+//  aastepanov_6 HW2
+//
+//  Created by Алексей Степанов on 2022-11-14.
+//
+
+import Foundation
